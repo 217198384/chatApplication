b7c53aa741aec7acb00c3e7875b793b3624766ad
gjjantjies9402 <80212402+gjjantjies9402@users.noreply.github.com>
2021-11-11 12:51:11 +0200

    part 2.1

---
+++ HomeScreen.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {Divider, List} from 'react-native-paper';
+
+import { kitty} from '../chatkitty';
+import Loading from '../components/Loading';
+
+export default function HomeScreen() {
+
+  const [channels, setChannels] = useState([]);
+  const [ loading, setLoading] = useState(true);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    kitty.getChannels({filter:{ joined: true}}).then((result) => {
+      if(!isCancelled){
+        setChannels(result.paginator.items);
+
+        if(loading){
+          setLoading(false);
+        }
+      }
+    });
+
+    return() => {
+      isCancelled = true;
+    };
+  }, [isFocused, loading]);
+
+  if(loading){
+    return <Loading />;
+  }
+   
+  return (
+    <View style={styles.container}>
+        <FlatList
+            data={channels}
+            keyExtractor={(item) => item.id.toString()}
+            ItemSeparatorComponent={() => <Divider />}
+            renderItem={({ item }) => (
+                <List.Item
+                    title={item.name}
+                    description={item.type}
+                    titleNumberOfLines={1}
+                    titleStyle={styles.listTitle}
+                    descriptionStyle={styles.listDescription}
+                    descriptionNumberOfLines={1}
+                    onPress={() => {
+                      // TODO navigate to a chat screen.
+                    }}
+                />
+            )}
+        />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
